846a17cd604a5f715bff4f2a5c3a64d5752e66bd
Tony Yang <superzoro168@gmail.com>
2023-09-02 16:31:54 +0800

    Initial commit

---
+++ src/main/java/com/tonyydl/springbootzerocrud/SpringBootZeroCrudApplication.java
@@ -0,0 +1,13 @@
+package com.tonyydl.springbootzerocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZeroCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZeroCrudApplication.class, args);
+    }
+
+}
